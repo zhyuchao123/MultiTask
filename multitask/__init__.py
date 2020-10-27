26493dd690160e90fedb60681fa2d3a1d5c2d4f9
Yuchao Zhang <u6715243@anu.edu.au>
2020-10-28 01:41:33 +0800

    relative path

--- multitask/__init__.py
+++ multitask/__init__.py
@@ -1,7 +1,15 @@
 __version__ = '0.1.0'
 
 
+def relativePath(abspath,degree):
+    abspath = str(abspath)
+    plist = abspath.split('/')
 
+    rtn = ''
+    for blk in (plist[-degree:]):
+        rtn +=blk
+        rtn+='\\'
+    return rtn[:-1]
 import requests
 import os,logger
 from get_data import download
@@ -11,11 +19,13 @@ logger.logger.info("finish downloading datasets......")
 
 # create reports folder
 reports_path = os.path.join(os.path.abspath(__file__ + "/../"), 'reports')
+reports_path = relativePath(reports_path,2)
 os.makedirs(reports_path, exist_ok=True)
 
 logger.logger.info("create a folder for reports")
 # create output models' file
-models_path = os.path.join(os.path.abspath(__file__ + "/../"), '/models/model_out')
+models_path = os.path.join(os.path.abspath(__file__ + "/../"), '/models/model_out_t2')
+models_path = (relativePath(models_path,2))
 
 os.makedirs(models_path, exist_ok=True)
 logger.logger.info("create a folder for trained models")
